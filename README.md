fae429f1bd31a0b921212dd1e9285ffbc3994478
Török Gábor <gabor@20y.hu>
2010-03-02 16:53:18 +0100

    gmaps-static-el initialized.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Display address on map inside an Emacs buffer
+
+![Displaying the user-given address on a map inside an Emacs buffer](http://farm5.static.flickr.com/4036/4400884771_743c935be8.jpg "M-x gmaps-static-show-address")
---
+++ gmaps-static.el
@@ -0,0 +1,29 @@
+;;; gmaps-static.el --- Display address on map inside an Emacs buffer
+
+;;; Commentary:
+;;
+;; Usage: M-x gmaps-static-show-address
+;;
+
+;;; History:
+;; 
+
+;;; Code:
+(defcustom gmaps-static-zoom-level 13
+  "Map zoom level.")
+
+(defvar gmaps-static-api-url-scheme "http://maps.google.com/maps/api/staticmap?zoom=%s&markers=size:small|color:black|%s|%s&size=500x300&sensor=false"
+  "Google Static Maps API URI scheme.")
+
+(defun gmaps-static-show-address (address zoom-level)
+  "Show ADDRESS on a map using Google Static Maps API.
+Argument ZOOM-LEVEL defaults to `gmaps-static-zoom-level'."
+  (interactive
+   (list (read-string "Address: ")
+         (read-number "Zoom level: " gmaps-static-zoom-level)))
+  (let ((address (url-hexify-string address)))
+    (url-mm-url (format gmaps-static-api-url-scheme zoom-level address address))))
+
+(provide 'gmaps-static)
+
+;;; gmaps-static.el ends here
